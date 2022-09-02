e2159821e44a759796a5381d7fbbc14565913aab
«Slavyanus <slavyanus@inbox.ru>
2022-09-03 01:41:05 +0700

    z2

---
+++ z2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+static void SumStringMatrix(int[,] matrix)
+{
+    int index = 0, minsum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.WriteLine($"Сумма {i + 1} строки = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        line += matrix[index, j] + " ";
+    }
+    Console.WriteLine($"Строка с минимальной суммой элементов равна {line}. ");
+    }
+
